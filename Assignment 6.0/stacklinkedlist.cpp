2655d24f38f226b2bf1f39582ed2d2d4d8ac3a73
Abhishek kumar <107607819+ErAbhishek1@users.noreply.github.com>
2023-02-16 14:33:23 +0530

    Create stacklinkedlist.cpp

---
+++ Assignment 6.0/stacklinkedlist.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+struct node{
+    int data;
+    struct node*next;
+};;
+struct node* top=0;
+struct node*newnode;
+struct node* head=0;
+
+
+void push(int x){
+    if(head=0){
+        newnode=head=top;
+        newnode=(struct node*)malloc(sizeof(struct node));
+    newnode->data=x;
+    newnode->next=top;
+    top=newnode;
+    }
+    else{
+    newnode=(struct node*)malloc(sizeof(struct node));
+    newnode->data=x;
+    newnode->next=top;
+    top=newnode;
+}
+}
+
+
+
+void display(){
+    struct node* temp;
+    temp=top;
+    if(top==0){
+        cout<<"empty"<<endl;
+    }
+    else{
+
+        while(temp!=0){
+        cout<<"->"<<temp->data;
+        temp=temp->next;
+    }
+}
+
+}
+
+int main() {
+push(1);
+push(2);
+push(3);
+push(4);
+push(5);
+display();
+
+
+
+
+ return 0;
+}
